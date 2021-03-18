8a990759684e73d8fbfc085777a96f3893960052
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-18 22:45:44 +0530

    Sixth Commit

--- app/src/main/java/com/example/projemanag/activity/IntroActivity.kt
+++ app/src/main/java/com/example/projemanag/activity/IntroActivity.kt
@@ -1,9 +1,11 @@
 package com.example.projemanag.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import com.example.projemanag.R
+import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity : AppCompatActivity() {
 
@@ -15,5 +17,9 @@ class IntroActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        btn_sign_up_intro.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/projemanag/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/projemanag/activity/SignUpActivity.kt
@@ -10,15 +10,15 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-
-
+        setupActionBar()
     }
 
     private fun setupActionBar(){
         setSupportActionBar(toolbar_sign_up_activity)
         val actionBar = supportActionBar
         if(actionBar != null){
-
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_keyboard_arrow_left_black_24dp)
         }
     }
 }

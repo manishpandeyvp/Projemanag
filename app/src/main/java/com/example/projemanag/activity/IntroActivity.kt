8bd51906de3d66cd66f661d26cb66dc6c77942e5
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-18 09:26:54 +0530

    Fourth Commit

--- app/src/main/java/com/example/projemanag/activity/IntroActivity.kt
+++ app/src/main/java/com/example/projemanag/activity/IntroActivity.kt
@@ -2,6 +2,7 @@ package com.example.projemanag.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.projemanag.R
 
 class IntroActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
 }

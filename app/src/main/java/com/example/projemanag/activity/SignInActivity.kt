a580f92c945ed5e83c192890a2aac0f685192983
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-19 20:13:54 +0530

    13th Commit

--- app/src/main/java/com/example/projemanag/activity/SignInActivity.kt
+++ app/src/main/java/com/example/projemanag/activity/SignInActivity.kt
@@ -2,15 +2,22 @@ package com.example.projemanag.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
 import com.example.projemanag.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
+
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        auth = FirebaseAuth.getInstance()
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -30,6 +37,29 @@ class SignInActivity : AppCompatActivity() {
 
         toolbar_sign_in_activity.setNavigationOnClickListener {
             onBackPressed()
+        }
+    }
+
+    private fun signInRegisteredUser(){
+        val email: String = et_email_sign_in.text.toString().trim{ it <= ' '}
+        val password: String = et_password_sign_in.text.toString().trim{ it <= ' '}
+
+        if(validateForm(email, password)){
+            
+        }
+    }
+
+    private fun validateForm(email: String, password: String): Boolean{
+        return when{
+            TextUtils.isEmpty(email) ->{
+                showErrorSnackBar("Please enter an email")
+                false
+            }
+            TextUtils.isEmpty(password) ->{
+                showErrorSnackBar("Please enter a password")
+                false
+            }
+            else -> return true
         }
     }
 }
--- app/src/main/java/com/example/projemanag/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/projemanag/activity/SignUpActivity.kt
@@ -6,6 +6,8 @@ import android.text.TextUtils
 import android.view.WindowManager
 import android.widget.Toast
 import com.example.projemanag.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : BaseActivity() {
@@ -45,7 +47,22 @@ class SignUpActivity : BaseActivity() {
         val password: String = et_password.text.toString().trim{ it <= ' '}
 
         if(validateForm(name, email,password)){
-            Toast.makeText(this@SignUpActivity, "Now we can register a user", Toast.LENGTH_SHORT).show()
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                hideProgressDialog()
+                if(task.isSuccessful){
+                    val firebaseUser : FirebaseUser = task.result!!.user!!
+                    val registeredEmail = firebaseUser.email
+                    Toast.makeText(
+                        this,
+                        "$name you have successfully registered the email address $registeredEmail",
+                        Toast.LENGTH_SHORT).show()
+                    FirebaseAuth.getInstance().signOut()
+                    finish()
+                } else {
+                    Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 

d1bea759b62590a420403ac9b9f05fedac173a3c
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-19 15:29:51 +0530

    12th Commit

--- app/src/main/java/com/example/projemanag/activity/BaseActivity.kt
+++ app/src/main/java/com/example/projemanag/activity/BaseActivity.kt
@@ -1,10 +1,14 @@
 package com.example.projemanag.activity
 
 import android.app.Dialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.example.projemanag.R
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.loading.*
 
@@ -46,5 +50,16 @@ open class BaseActivity : AppCompatActivity() {
             resources.getString(R.string.please_click_back_again_to_exit),
             Toast.LENGTH_SHORT
         ).show()
+
+        Handler().postDelayed({
+            doubleBackToExitPressedOnce = false
+        }, 2000)
+    }
+
+    fun showErrorSnackBar(message : String){
+        val snackBar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+        snackBarView.setBackgroundColor(ContextCompat.getColor(this, R.color.snackbar_error_color))
+        snackBar.show()
     }
 }
--- app/src/main/java/com/example/projemanag/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/projemanag/activity/SignUpActivity.kt
@@ -2,11 +2,13 @@ package com.example.projemanag.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.projemanag.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +32,38 @@ class SignUpActivity : AppCompatActivity() {
 
         toolbar_sign_up_activity.setNavigationOnClickListener {
             onBackPressed()
+        }
+
+        btn_sign_up.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    private fun registerUser(){
+        val name: String = et_name.text.toString().trim{ it <= ' '}
+        val email: String = et_email.text.toString().trim{ it <= ' '}
+        val password: String = et_password.text.toString().trim{ it <= ' '}
+
+        if(validateForm(name, email,password)){
+            Toast.makeText(this@SignUpActivity, "Now we can register a user", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validateForm(name: String, email: String, password: String): Boolean{
+        return when{
+            TextUtils.isEmpty(name) ->{
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+            TextUtils.isEmpty(email) ->{
+                showErrorSnackBar("Please enter an email")
+                false
+            }
+            TextUtils.isEmpty(password) ->{
+                showErrorSnackBar("Please enter a password")
+                false
+            }
+            else -> return true
         }
     }
 }

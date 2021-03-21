43ecf95c6c851594f2b442981a2abaf01a56f845
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-21 22:37:28 +0530

    20th Commit

--- app/src/main/java/com/example/projemanag/activity/MainActivity.kt
+++ app/src/main/java/com/example/projemanag/activity/MainActivity.kt
@@ -2,12 +2,42 @@ package com.example.projemanag.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
 import com.example.projemanag.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(toolbar_main_Activity)
+        toolbar_main_Activity.setNavigationIcon(R.drawable.ic_menu_black_24dp)
+
+        toolbar_main_Activity.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer(){
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            doubleBackToExit()
+        }
     }
 }

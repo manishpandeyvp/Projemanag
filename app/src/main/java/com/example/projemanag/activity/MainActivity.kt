dd10c9ad538b91b76fe1080217382a382cf7817d
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-21 22:47:54 +0530

    21st Commit

--- app/src/main/java/com/example/projemanag/activity/MainActivity.kt
+++ app/src/main/java/com/example/projemanag/activity/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.projemanag.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import com.example.projemanag.R
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupActionBar()
+
+        nav_view.setNavigationItemSelectedListener(this)
     }
 
     private fun setupActionBar(){
@@ -39,5 +46,22 @@ class MainActivity : BaseActivity() {
         } else {
             doubleBackToExit()
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_my_profile -> {
+                Toast.makeText(this@MainActivity, "My Profile", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }

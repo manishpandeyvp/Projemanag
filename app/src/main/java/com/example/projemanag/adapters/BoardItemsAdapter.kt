99d7a1ceecdc19a33d7e320eba59596af359dede
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-24 21:22:42 +0530

    39th Commit

--- app/src/main/java/com/example/projemanag/adapters/BoardItemsAdapter.kt
+++ app/src/main/java/com/example/projemanag/adapters/BoardItemsAdapter.kt
@@ -1,4 +1,45 @@
 package com.example.projemanag.adapters
 
-class BoardItemsAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.projemanag.R
+import com.example.projemanag.models.Board
+import kotlinx.android.synthetic.main.activity_create_board.view.*
+import kotlinx.android.synthetic.main.item_board.view.*
+
+open class BoardItemsAdapter(
+    private val context: Context,
+    private val list: ArrayList<Board>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_board, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+        if(holder is MyViewHolder){
+            Glide.with(context)
+                .load(model.image)
+                .centerCrop()
+                .placeholder(R.drawable.ic_board_place_holder)
+                .into(holder.itemView.iv_board_image)
+
+            holder.itemView.tv_name.text = model.name
+            holder.itemView.tv_created_by.text = "Created by: ${model.createdBy}"
+
+            holder.itemView.setOnClickListener {
+                
+            }
+        }
+    }
+
+    private class MyViewHolder(view: View): RecyclerView.ViewHolder(view)
+
 }

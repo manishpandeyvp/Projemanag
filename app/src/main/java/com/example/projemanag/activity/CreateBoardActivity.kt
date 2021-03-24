a22522bffca00cd8f81c4bf97d7fc7833bc9fb41
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-24 10:21:28 +0530

    32nd Commit

--- app/src/main/java/com/example/projemanag/activity/CreateBoardActivity.kt
+++ app/src/main/java/com/example/projemanag/activity/CreateBoardActivity.kt
@@ -1,12 +1,25 @@
 package com.example.projemanag.activity
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.example.projemanag.R
+import com.example.projemanag.utils.Constants
 import kotlinx.android.synthetic.main.activity_create_board.*
+import kotlinx.android.synthetic.main.activity_my_profile.*
+import java.io.IOException
 
 class CreateBoardActivity : AppCompatActivity() {
+    private var mSelectedImageFileUri : Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +31,14 @@ class CreateBoardActivity : AppCompatActivity() {
         )
 
         setupActionBar()
+
+        iv_user_image_create_board.setOnClickListener {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                Constants.showImageChooser(this)
+            }else{
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), Constants.READ_STORAGE_PERMISSION_CODE)
+            }
+        }
     }
 
     private fun setupActionBar(){
@@ -30,6 +51,38 @@ class CreateBoardActivity : AppCompatActivity() {
 
         toolbar_create_board_activity.setNavigationOnClickListener {
             onBackPressed()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == Constants.READ_STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Constants.showImageChooser(this)
+            } else {
+                Toast.makeText(this, "Oops, you just denied the permission for storage!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == Constants.PICK_IMAGE_REQUEST_CODE && data!!.data != null){
+            mSelectedImageFileUri = data.data
+            try {
+                Glide
+                    .with(this)
+                    .load(mSelectedImageFileUri).centerCrop()
+                    .placeholder(R.drawable.ic_person_black_24dp)
+                    .into(iv_user_image_create_board)
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+
         }
     }
 }

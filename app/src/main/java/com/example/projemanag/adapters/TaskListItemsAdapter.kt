7cd51a49f65e04b4f39341725b4f4b7d1133123c
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-27 09:37:00 +0530

    57th Commit

--- app/src/main/java/com/example/projemanag/adapters/TaskListItemsAdapter.kt
+++ app/src/main/java/com/example/projemanag/adapters/TaskListItemsAdapter.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projemanag.R
 import com.example.projemanag.activity.TaskListActivity
@@ -113,6 +114,12 @@ open class TaskListItemsAdapter (
                     Toast.makeText(context, "Please enter a card name!", Toast.LENGTH_SHORT).show()
                 }
             }
+
+            holder.itemView.rv_card_list.layoutManager = LinearLayoutManager(context)
+            holder.itemView.rv_card_list.setHasFixedSize(true)
+
+            val adapter = CardListItemsAdapter(context, model.cards)
+            holder.itemView.rv_card_list.adapter = adapter
         }
     }
 

7c866102c14164f7c5d020df5270fd98d959d4c4
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-27 15:59:37 +0530

    60th Commit

---
+++ app/src/main/java/com/example/projemanag/adapters/MemberListItemsAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.projemanag.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.projemanag.R
+import com.example.projemanag.models.User
+import kotlinx.android.synthetic.main.item_member.view.*
+
+open class MemberListItemsAdapter(
+    private val context: Context,
+    private val list: ArrayList<User>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.item_member,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+
+        if(holder is MyViewHolder){
+            Glide.with(context)
+                .load(model.image)
+                .centerCrop()
+                .placeholder(R.drawable.ic_board_place_holder)
+                .into(holder.itemView.iv_member_image)
+
+            holder.itemView.tv_member_name.text = model.name
+            holder.itemView.tv_member_email.text = model.email
+        }
+    }
+
+
+    private class MyViewHolder(view: View): RecyclerView.ViewHolder(view)
+}

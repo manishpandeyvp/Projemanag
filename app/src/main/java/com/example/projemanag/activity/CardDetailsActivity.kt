1d8a7ccd8577a8383e7485339b2bf26de9fe28b1
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-28 20:31:36 +0530

    67th Commit

--- app/src/main/java/com/example/projemanag/activity/CardDetailsActivity.kt
+++ app/src/main/java/com/example/projemanag/activity/CardDetailsActivity.kt
@@ -2,11 +2,18 @@ package com.example.projemanag.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.WindowManager
 import com.example.projemanag.R
+import com.example.projemanag.models.Board
+import com.example.projemanag.utils.Constants
 import kotlinx.android.synthetic.main.activity_card_details.*
 
 class CardDetailsActivity : AppCompatActivity() {
+
+    private lateinit var mBoardDetails: Board
+    private var mTaskListPosition = -1
+    private var mCardPosition = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +23,11 @@ class CardDetailsActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-
+        getIntentData()
         setupActionBar()
+
+        et_name_card_details.setText(mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name)
+        et_name_card_details.setSelection(et_name_card_details.text.toString().length)
     }
 
     private fun setupActionBar(){
@@ -28,10 +38,27 @@ class CardDetailsActivity : AppCompatActivity() {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_back_white)
         }
 
-//        toolbar_card_details_activity.text = mBoardDetails.name
+        tv_card_details.text = mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].name
 
         toolbar_card_details_activity.setNavigationOnClickListener {
             onBackPressed()
         }
+    }
+
+    private fun getIntentData(){
+        if(intent.hasExtra(Constants.BOARD_DETAIL)){
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+        }
+        if(intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)){
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION, -1)
+        }
+        if(intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)){
+            mCardPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_delete_card, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
--- app/src/main/java/com/example/projemanag/activity/TaskListActivity.kt
+++ app/src/main/java/com/example/projemanag/activity/TaskListActivity.kt
@@ -56,7 +56,7 @@ class TaskListActivity : BaseActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK && requestCode == MEMBERS_REQUEST_CODE){
+        if(resultCode == Activity.RESULT_OK && requestCode == MEMBERS_REQUEST_CODE || requestCode == CARD_DETAILS_REQUEST_CODE){
             showProgressDialog(resources.getString(R.string.please_wait))
             FirestoreClass().getBoardDetails(this, mBoardDocumentId)
         } else {
@@ -160,10 +160,11 @@ class TaskListActivity : BaseActivity() {
         intent.putExtra(Constants.BOARD_DETAIL, mBoardDetails)
         intent.putExtra(Constants.TASK_LIST_ITEM_POSITION, taskListPosition)
         intent.putExtra(Constants.CARD_LIST_ITEM_POSITION, cardPosition)
-        startActivity(intent)
+        startActivityForResult(intent, CARD_DETAILS_REQUEST_CODE)
     }
 
     companion object{
         const val MEMBERS_REQUEST_CODE : Int = 13
+        const val CARD_DETAILS_REQUEST_CODE: Int = 14
     }
 }

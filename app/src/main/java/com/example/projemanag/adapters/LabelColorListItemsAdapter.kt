5f1efdf0d4d14694343de7242119ad2952269c6d
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-29 20:34:01 +0530

    72nd Commit

---
+++ app/src/main/java/com/example/projemanag/adapters/LabelColorListItemsAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.projemanag.adapters
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projemanag.R
+import kotlinx.android.synthetic.main.item_label_color.view.*
+
+open class LabelColorListItemsAdapter(
+    private val context: Context,
+    private val list: ArrayList<String>,
+    private val mSelectedColor: String
+): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.item_label_color, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = list[position]
+        if(holder is MyViewHolder){
+            holder.itemView.view_main.setBackgroundColor(Color.parseColor(item))
+            if(item == mSelectedColor){
+                holder.itemView.iv_selected_color.visibility = View.VISIBLE
+            }else{
+                holder.itemView.iv_selected_color.visibility = View.GONE
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onCLick(position: Int, color: String)
+    }
+
+    private class MyViewHolder(view: View): RecyclerView.ViewHolder(view)
+}

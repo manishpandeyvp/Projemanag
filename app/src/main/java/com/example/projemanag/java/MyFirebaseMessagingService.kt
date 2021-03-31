3d606a55dc5969e02c8439de4f4462bb7ac15889
Manish Pandey <2018140@iiitdmj.ac.in>
2021-03-31 14:42:56 +0530

    87th Commit

--- app/src/main/java/com/example/projemanag/java/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/projemanag/java/MyFirebaseMessagingService.kt
@@ -1,6 +1,17 @@
 package com.example.projemanag.java
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.projemanag.R
+import com.example.projemanag.activity.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -28,6 +39,37 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
 
     private fun sendRegistrationToServer(token: String?){
         //Implement
+    }
+
+    private fun sendNotification(messageBody: String){
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingActivity = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val channelId = this.resources.getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(
+            this,
+            channelId
+        ).setSmallIcon(R.drawable.ic_android_black_24dp)
+            .setContentTitle("Title")
+            .setContentText("Message")
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingActivity)
+
+        val notificationManager = getSystemService(
+            Context.NOTIFICATION_SERVICE
+        ) as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(
+                channelId,
+                "Channel Projemanag Title",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0, notificationBuilder.build())
     }
 
     companion object{
